dce60c3c4825db6b233df3a98bf7b526c0983c98
Beatriz-Lima-Beserra <beatrizlimaa37@gmail.com>
2022-07-19 20:23:41 -0300

    Primeiro exerício de Classes

---
+++ ExercícioFigura/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExercícioFigura.src;
+using System;
+
+namespace ExercícioFigura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Garrafas da Catarina:");
+
+            Garrafa barbie = new Garrafa("Barbie","Rosa", "Pequena", "Redonda");
+            Garrafa ironman = new Garrafa("Iron Man", "Vermelho", "Grande", "Quadrada");
+            Garrafa spiderman = new Garrafa("Spider Man","Azul", "Grande", "Redonda");
+            Garrafa polly = new Garrafa("Polly","Laranja", "Grande", "Rendonda");
+
+
+
+            Console.WriteLine(barbie.Tostring());
+            Console.WriteLine(ironman.Tostring());
+            Console.WriteLine(spiderman.Tostring());
+            Console.WriteLine(polly.Tostring());
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ExercícioFigura/src/Garrafa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercícioFigura.src
+{
+	public class Garrafa
+	{
+        public string Personagem { get; set; }
+		public string Cor { get; set; }
+
+		public string Tamanho { get; set; }
+
+		public string Formato { get; set; }
+
+        public Garrafa()
+        {
+
+        }
+
+        public Garrafa(string personagem, string cor, string tamanho, string formato)
+        {
+            Personagem = personagem;
+            Cor = cor;
+            Tamanho = tamanho;
+            Formato = formato;
+        }
+        public string Tostring()
+        {
+            return $"Personagem: {Personagem} \n" +
+                   $"Cor: {Cor} \n" +
+                   $"Tamanho: {Tamanho} \n" +
+                   $"Formato: {Formato} \n";
+
+        }
+
+        
+    }
+		
+}
